ba5e29781e36cb3bdb32c7ef92057e18917aa5f5
aberob35 <arobin@pugetsound.edu>
2023-01-23 18:51:13 -0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -7,6 +7,11 @@
 
 #ifndef MENU_H_
 #define MENU_H_
+#define  MENU_STATS 1
+#define  MENU_HISTO 2
+#define  MENU_INPUT 3
+#define  MENU_EXIT 4
+
 
 // function declarations for the menu below
 int getMenuOption();
